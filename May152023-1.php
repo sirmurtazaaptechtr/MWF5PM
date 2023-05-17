bf910b0581e047a21271e6a009b82dff5fbe1009
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-05-17 17:41:55 +0500

    15-May-2023

---
+++ May152023-1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // for single line comment 
+        # for single line comment
+
+        /*
+        Jitni Bhi Line likho 
+        koi masla nhi hai
+        in lines ko compiler nahi 
+        perhe ga
+        */
+
+        echo "this is php 8.0";
+
+        $number = 12;
+
+        echo "<br>" . $number;
+        echo "<br> The Result is " . $number;
+        echo "<br> The Result is $number";
+
+        $lang = "PHP 8.0";
+
+        echo "<br> I am working with $lang.";
+
+        $num1 = 15;
+        $num2 = 5;
+        
+        # $res = $num1 + $num2;
+        # echo "<br>$num1 + $num2 = $res";
+
+        echo "<br>$num1 + $num2 = " . $num1 + $num2;
+        echo "<br>$num1 - $num2 = " . $num1 - $num2;
+        echo "<br>$num1 x $num2 = " . $num1 * $num2;
+        echo "<br>$num1 / $num2 = " . $num1 / $num2;
+    ?>    
+
+</body>
+</html>
